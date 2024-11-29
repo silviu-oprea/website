5acf45e543401f8ae22846611747326cec97ef3b
Silviu Oprea <silviu.oprea@ed.ac.uk>
2024-11-29 17:54:55 +0000

    Quartz sync: Nov 29, 2024, 5:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -76,7 +76,7 @@ const config: QuartzConfig = {
     ],
     filters: [
       Plugin.RemoveDrafts(),
-      Plugin.ExplicitPublish()
+      // Plugin.ExplicitPublish()
     ],
     emitters: [
       Plugin.AliasRedirects(),
